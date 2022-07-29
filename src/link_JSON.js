4b3a8ffc8caf54654aa9a2d30b9ac9f040fbd23f
AbduqahhorNorimmatov <AbduqahhorDeveloper@gmail.com>
2022-07-29 11:58:44 +0500

    push

--- src/link_JSON.js
+++ src/link_JSON.js
@@ -2,7 +2,14 @@ import doctor1 from "./images/doctor1.jpg"
 import doctor2 from "./images/doctor2.jpg"
 import doctor3 from "./images/doctor3.jpg"
 import doctor4 from "./images/doctor4.jpg"
-
+import facebook from "./images/facebook.jpg"
+import instagram from "./images/instagram.jpg"
+import youtube from "./images/youtube.jpg"
+import telegram from "./images/telegram.png"
+import twitter from "./images/twitter.jpg"
+import commenter1 from "./images/commenter1.jpg"
+import commenter2 from "./images/commenter2.jpg"
+import commenter3 from "./images/commenter3.jpg"
 
 
 
@@ -175,5 +182,49 @@ export const ourTeam = [
                 url: "#"
             }
         ] 
+    }
+]
+
+export const social_images = [
+    { 
+        type: "facebook",
+        img: facebook,
+    },
+    { 
+        type: "instagram",
+        img: instagram,
+    },
+    { 
+        type: "telegram",
+        img: telegram,
+    },
+    { 
+        type: "youtube",
+        img: youtube,
+    },
+    { 
+        type: "twitter",
+        img: twitter,
+    }
+]
+
+export const commenters = [
+    {
+        img: commenter1,
+        name: "Alan Zara dilan",
+        address: "New York, America, USA",
+        comment: "Telehealth is fueled by digital technologies and DocTime telemedicine app has brought a great revolution in medical services specially an overpopulated country where virtual chamber can create at anywhere thoughout country"
+    },
+    {
+        img: commenter3,
+        name: "Alex Maxwell",
+        address: "San Francisco, USA",
+        comment: "I visited my PCP for abdominal pain. My doctor told me that I need to see Gastroenterologist. Earlier I used to call multiple offices to schedule an appointment that suites my time. Can create at anywhere thoughout country"
+    },
+    {
+        img: commenter2,
+        name: "Los Angeles, USA",
+        address: "New York, America, USA",
+        comment: "HealthCare is enlightens my superpower each and every time...! I find DocTime a brand-able telemedicine platform with virtual waiting room, video consultation, e-prescription, beautifully planned user interface ;)"
     }
 ]
